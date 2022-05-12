8a2223eab08c972a050428132fd6a1d29eac3d5a
ManhAnNguyen <thungantn1999@gmail.com>
2022-05-12 08:15:40 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 import { ImHome } from "react-icons/im";
 import { MdHomeMini } from "react-icons/md";
+import Item from "./components/Item";
 
 const inititalValue = {
   name: "",
@@ -26,13 +27,19 @@ const App = () => {
       return;
     }
 
-    setData([...data, newItem]);
+    setData([...data, { ...newItem, id: new Date() }]);
 
     setNewItem(inititalValue);
     setShowForm(false);
   };
 
-  console.log(data);
+  const handleDelete = (id) => {
+    setData(data.filter((d) => d.id !== id));
+    // console.log(id);
+  };
+
+  // console.log(data);
+
   return (
     <>
       <span className="icon">
@@ -88,7 +95,25 @@ const App = () => {
             />
           )}
         </div>
-        <div className="list-expense"></div>
+        {data.length === 0 ? (
+          <h1 style={{ color: "white" }}>0 Item</h1>
+        ) : (
+          <div className="list-expense">
+            <div className="filter"></div>
+            <div className="list-group">
+              {data.map((d, index) => (
+                <Item
+                  name={d.name}
+                  date={d.date}
+                  amount={d.amount}
+                  key={index}
+                  handleDelete={handleDelete}
+                  id={d.id}
+                />
+              ))}
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
