d40a25d82ed872dba3a6b504025b3d07d5bf71a0
ManhAnNguyen <thungantn1999@gmail.com>
2022-05-16 20:40:39 +0700

    add day8

--- src/App.js
+++ src/App.js
@@ -1,121 +1,44 @@
-import React, { useState } from "react";
-import "./App.css";
-import Button from "./components/Button";
-import Input from "./components/Input";
-import { ImHome } from "react-icons/im";
-import { MdHomeMini } from "react-icons/md";
-import Item from "./components/Item";
+import React, { createContext, useReducer } from "react";
+import HomeWork from "./homework";
+import { useEffect, useState } from "react";
+import TodoList from "./components/TodoList";
 
-const inititalValue = {
-  name: "",
-  amount: "",
-  date: "",
+const initialValue = {
+  color: "green",
+};
+
+export const TodoContext = createContext();
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "CHANGE":
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 const App = () => {
-  const [showForm, setShowForm] = useState(false);
-  const [newItem, setNewItem] = useState(inititalValue);
-  const { name, amount, date } = newItem;
+  const [count, dispatch] = useReducer(countReducer, 1);
 
-  const [data, setData] = useState([]);
+  console.log("count :", count);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!name || !amount || !date) {
-      alert("Please full fill !!");
-      return;
-    }
-
-    setData([...data, { ...newItem, id: new Date() }]);
-
-    setNewItem(inititalValue);
-    setShowForm(false);
-  };
-
-  const handleDelete = (id) => {
-    setData(data.filter((d) => d.id !== id));
-    // console.log(id);
-  };
-
-  // console.log(data);
+  const increment = () => dispatch({ type: "INCREMENT" });
+  const decrement = () => dispatch({ type: "DECREMENT" });
+  const change = () => dispatch({ type: "CHANGE", payload: 1000 });
 
   return (
-    <>
-      <span className="icon">
-        <ImHome fontSize="40px" color="white" />
-      </span>
-      <main id="section">
-        <div className="add-form">
-          {showForm ? (
-            <form onSubmit={handleSubmit}>
-              <div className="input-group">
-                <Input
-                  placeholder="Enter name here"
-                  label="Name"
-                  valueInput={name}
-                  onChangeInput={(e) =>
-                    setNewItem({ ...newItem, name: e.target.value })
-                  }
-                />
-                <Input
-                  placeholder="Enter amount here"
-                  label="Amount"
-                  typeInput="number"
-                  valueInput={amount}
-                  onChangeInput={(e) =>
-                    setNewItem({ ...newItem, amount: e.target.value })
-                  }
-                />
-                <Input
-                  placeholder="Date"
-                  typeInput="date"
-                  label="Date"
-                  valueInput={date}
-                  onChangeInput={(e) =>
-                    setNewItem({ ...newItem, date: e.target.value })
-                  }
-                />
-              </div>
-
-              <div className="button-group">
-                <Button colorButton="violet" text="ADD" />
-                <Button
-                  colorButton="gray"
-                  text="CANCEL"
-                  onClickButton={() => setShowForm(false)}
-                />
-              </div>
-            </form>
-          ) : (
-            <Button
-              onClickButton={() => setShowForm(true)}
-              colorButton={"violet"}
-              text="ADD NEW EXPENSE"
-            />
-          )}
-        </div>
-        {data.length === 0 ? (
-          <h1 style={{ color: "white" }}>0 Item</h1>
-        ) : (
-          <div className="list-expense">
-            <div className="filter"></div>
-            <div className="list-group">
-              {data.map((d, index) => (
-                <Item
-                  name={d.name}
-                  date={d.date}
-                  amount={d.amount}
-                  key={index}
-                  handleDelete={handleDelete}
-                  id={d.id}
-                />
-              ))}
-            </div>
-          </div>
-        )}
-      </main>
-    </>
+    <TodoContext.Provider value={initialValue}>
+      <TodoList />
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
+      <button onClick={change}>Change to 1000</button>
+      <h1>Count : {count}</h1>
+    </TodoContext.Provider>
   );
 };
 
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+};
+
+export default TodoList;
